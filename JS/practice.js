daedbe443958ca1bf35afcf9feb3eac167ffaa8e
Sennarin <Kaikispin@gmail.com>
2020-07-22 18:44:03 +0300

    initial practice

---
+++ JS/practice.js
@@ -0,0 +1,12 @@
+
+
+const str = "test";
+const arr = [1, 2, 4];
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
+
+const logg = "hello world!";
+
+console.log(logg.slice(6, 11));
---
+++ JS/script.js
@@ -0,0 +1,67 @@
+let numberOfFilms;
+
+function start () {
+    numberOfFilms = +prompt('How many movies have you watched?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms) ) {
+        numberOfFilms = +prompt('How many movies have you watched?', '');
+    }
+}
+
+start();
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('one of the last movies?', ''),
+              b = prompt('how much did you like it?', '');
+        
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('cool dude');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
+
+rememberMyFilms();
+
+
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('not many films');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+        console.log('medium amount of films');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('a lot of films nigga');
+    } else {
+        console.log('your mom gae');
+    }
+}
+
+detectPersonalLevel();
+
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.private);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++){
+        personalMovieDB.genres[i - 1] = prompt(`your favorite genre under the number ${i}`);
+    }
+}
+
+writeYourGenres();
